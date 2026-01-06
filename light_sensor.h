a0f35648997dde9f7e2048a876d04dd88aa6ab8b
Jordana711731 <endombajordana@gmail.com>
2026-01-06 12:17:01 +0100

    Create light_sensor.h

---
+++ light_sensor.h
@@ -0,0 +1,13 @@
+/*
+ * light_sensor.h
+ *
+ *  Created on: 6 janv. 2026
+ *      Author: 63790654
+ */
+
+#ifndef LIGHT_SENSOR_H_
+#define LIGHT_SENSOR_H_
+
+int return_light_sensor();
+
+#endif /* LIGHT_SENSOR_H_ */
