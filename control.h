3019abe613803b77e1e5d50fa0cdd0cd302444e0
Baptiste-micl <163674826+Baptiste-micl@users.noreply.github.com>
2026-01-06 11:45:45 +0100

    Create control.h with motor control function declarations

---
+++ control.h
@@ -0,0 +1,27 @@
+/*
+ * control.h
+ *
+ *  Created on: 6 janv. 2026
+ *      Author: 36843400
+ */
+
+#ifndef CONTROL_H_
+#define CONTROL_H_
+
+void moteur_A_avant(void);
+
+void moteur_A_arriere(void);
+
+void moteur_B_avant(void);
+
+void moteur_B_arriere(void);
+
+void avancer(void);
+
+void tourner_gauche(void)
+
+void tourner_droite(void)
+
+void stop_moteurs(void)
+
+#endif /* CONTROL_H_ */
