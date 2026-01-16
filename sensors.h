5d04e831ed357d7dd8e5821a2589fca457c0966f
Jordana711731 <endombajordana@gmail.com>
2026-01-16 19:10:52 +0100

    Update sensors.h

--- sensors.h
+++ sensors.h
@@ -1,17 +1,24 @@
 #ifndef SENSORS_H
 #define SENSORS_H
 
-// DÈfinitions des capteurs
+// D√©finitions des capteurs
 #define CAPTEUR_GAUCHE BIT1    // P1.1
-#define CAPTEUR_DROITE BIT0    // P1.0
+#define CAPTEUR_DROITE BIT2    // P1.2
+
 #define INFRA_RED BIT2
 #define VREF 5.0
 #define ADC_MAX 1023.0
-
-
-
-// Seuil de dÈtection d'obstacle
+// Seuil de d√©tection d'obstacle
 #define SEUIL_OBSTACLE 150
+
+// Capteur de lumi√®re
+#define CAPTEUR_LIGHT         BIT3
+#define CAPTEUR_LIGHT_ANALOG  3
+#define SEUIL_LUMIERE         1000
+
+
+
+
 
 /**
  * Lit la distance du capteur d'obstacle via l'ADC
@@ -20,15 +27,22 @@
 unsigned int lire_distance(void);
 
 /**
- * Lit l'Ètat du capteur gauche
- * @return 1 si ligne dÈtectÈe, 0 sinon
+ * Lit l'√©tat du capteur gauche
+ * @return 1 si ligne d√©tect√©e, 0 sinon
  */
 unsigned char lire_capteur_gauche(void);
 
 /**
- * Lit l'Ètat du capteur droit
- * @return 1 si ligne dÈtectÈe, 0 sinon
+ * Lit l'√©tat du capteur droit
+ * @return 1 si ligne d√©tect√©e, 0 sinon
  */
 unsigned char lire_capteur_droite(void);
+
+/*
+ * Capteur de lumi√®re renvoie la valeur lu
+ */
+
+unsigned int return_light_sensor(void);
+
 
 #endif
