88ce3157ff6db2bc2e9ddfd15d59b5c45d54b298
Baptiste-micl <163674826+Baptiste-micl@users.noreply.github.com>
2026-01-19 09:23:12 +0100

    Refactor distance calculation in infra_red_sensor.c
    Removed unnecessary variable distance_mm and simplified return statement.

--- infra_red_sensor.c
+++ infra_red_sensor.c
@@ -11,32 +11,23 @@ unsigned int return_distance_mm(void)
     unsigned int adc_value;
     float voltage;
     float distance_cm;
-    unsigned int distance_mm;
 
-    P1DIR &= ~INFRA_RED;   // Entrée (capteur)
-    ADC_init();
+    P1DIR &= ~INFRA_RED;   // entrée capteur IR
 
-    ADC_Demarrer_conversion(1);
+    ADC_Demarrer_conversion(0);
     adc_value = ADC_Lire_resultat();
 
-    // Conversion ADC -> tension
     voltage = (adc_value * VREF) / ADC_MAX;
 
-    // Sécurité (évite division par zéro)
     if (voltage < 0.45)
         voltage = 0.45;
 
-    // Formule issue de la datasheet (approximation)
     distance_cm = 27.86 / (voltage - 0.42);
 
-    // Limitation plage capteur
     if (distance_cm < 4)  distance_cm = 4;
     if (distance_cm > 30) distance_cm = 30;
 
-    // Conversion en millimètres
-    distance_mm = (unsigned int)(distance_cm * 10);
-
-    return distance_mm;
+    return (unsigned int)(distance_cm * 10); // mm
 }
 
 void main_test_infra_rouge() {
